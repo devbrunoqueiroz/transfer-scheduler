0ea9df2b38d706cd2f43385791926a4b3a20e692
devbrunoqueiroz <devbruno.queiroz@gmail.com>
2026-06-27 07:55:03 -0300

    feat: cria home e routing

---
+++ frontend/src/app/features/transfers/pages/home/home.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { TransferResponse } from '../../../../core/models/transfer-response';
+import { TransferService } from '../../../../core/services/transfer-service';
+import { Transfers } from '../../components/transfers-form/transfers';
+import { TransfersList } from '../../components/transfers-list/transfers-list';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    Transfers,
+    TransfersList
+  ],
+
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home implements OnInit {
+
+  transfers: TransferResponse[] = [];
+
+  constructor(private transferService: TransferService) {}
+
+  ngOnInit(): void {
+    this.loadTransfers();
+  }
+
+  loadTransfers(): void {
+    this.transferService.getAll()
+      .subscribe(transfers => {
+        this.transfers = transfers;
+      });
+  }
+}
