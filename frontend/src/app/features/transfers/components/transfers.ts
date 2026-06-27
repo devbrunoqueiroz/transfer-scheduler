513f7662bdb6ca2bc66ba5789b8c4cd017fc410e
devbrunoqueiroz <devbruno.queiroz@gmail.com>
2026-06-27 06:53:27 -0300

    feat: cria estrutura de pastas e componente de transferencia

---
+++ frontend/src/app/features/transfers/components/transfers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfers',
+  imports: [],
+  templateUrl: './transfers.html',
+  styleUrl: './transfers.scss',
+})
+export class Transfers {}
